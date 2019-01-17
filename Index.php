2ae2380147ff0a3fd0ac99d4b4c90f8bdbec8964
alexander10122 <36293099+alexander10122@users.noreply.github.com>
2019-01-17 14:44:27 +0100

    basic html toegevoegd & css gekoppeld
    ik heb een basic html toegevoegd en ik heb ook daarbij gelijk de style.css gelinkt met de html/php file voor stijling

---
+++ Index.php
@@ -0,0 +1,15 @@
+
+
+
+<!DOCTYPE html 5.0>
+<head>
+	</head>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<header>
+	</header>
+<html>
+<body>
+</body>
+<footer>
+</footer>
+</html>
